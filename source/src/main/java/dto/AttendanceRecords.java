a34516aee35efc1069db4d92a957ec0991b4afbe
plusdojo2025-Nakano <nakano-ayame-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 16:37:42 +0900

    DTO編集
    以下のDTOを編集
    ・出欠記録
    ・授業
    ・面談

--- source/src/main/java/dto/AttendanceRecords.java
+++ source/src/main/java/dto/AttendanceRecords.java
@@ -1,8 +1,101 @@
 package dto;
 
+import java.util.Date;
+
 public class AttendanceRecords {
-	int recordId;
-	int studentid;
-	int classId;
+	private int recordId;
+	private int studentId;
+	private int classId;
+	private Date date;
+	private String period;
+	private int subjectId;
+	private String status;
+	private String remarks;
+	
+	public AttendanceRecords(int recordId, int studentId, int classId, Date date, String period, int subjectId, String status, String remarks) {
+		this.recordId = recordId;
+		this.studentId = studentId;
+		this.classId = classId;
+		this.date = date;
+		this.period = period;
+		this.subjectId = subjectId;
+		this.status = status;
+		this.remarks = remarks;
+	}
+	
+	public AttendanceRecords() {
+		this.recordId = -1;
+		this.studentId = -1;
+		this.classId = -1;
+		this.date = new Date();
+		this.period = "";
+		this.subjectId = -1;
+		this.status = "";
+		this.remarks = "";
+	}
+
+	public int getRecordId() {
+		return recordId;
+	}
+
+	public void setRecordId(int recordId) {
+		this.recordId = recordId;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public int getClassId() {
+		return classId;
+	}
+
+	public void setClassId(int classId) {
+		this.classId = classId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
 	
 }
--- source/src/main/java/dto/Interview.java
+++ source/src/main/java/dto/Interview.java
@@ -1,5 +1,90 @@
 package dto;
 
-public class Interview {
+import java.util.Date;
 
+public class Interview {
+	private int interviewId;
+	private int teacherId;
+	private Date date;
+	private int studentId;
+	private String contents;
+	private String remarks;
+	private int subjectId;
+	
+	public Interview(int interviewId, int teacherId, Date date, int studentId, String contents, String remarks, int subjectId) {
+		this.interviewId = interviewId;
+		this.teacherId = teacherId;
+		this.date = date;
+		this.studentId = studentId;
+		this.contents = contents;
+		this.remarks = remarks;
+		this.subjectId = subjectId;
+	}
+	
+	public Interview() {
+		this.interviewId = -1;
+		this.teacherId = -1;
+		this.date = new Date();
+		this.studentId = -1;
+		this.contents = "";
+		this.remarks = "";
+		this.subjectId = -1;
+	}
+
+	public int getInterviewId() {
+		return interviewId;
+	}
+
+	public void setInterviewId(int interviewId) {
+		this.interviewId = interviewId;
+	}
+
+	public int getTeacherId() {
+		return teacherId;
+	}
+
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+	
 }
