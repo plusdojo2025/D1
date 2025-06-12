2afbdff12dfd69b42605b88b39180285450f3bd6
plusdojo2025-aoyagi-kakeya <aoyagi-kakeya-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 11:55:16 +0900

    Update Teacher.java
    Teacher.DTO

--- source/src/main/java/dto/Teacher.java
+++ source/src/main/java/dto/Teacher.java
@@ -1,5 +1,40 @@
 package dto;
 
-public class Teacher {
+import java.io.Serializable;
 
+public class Teacher implements Serializable {
+	private int teacherId;		//教員ID
+	private String name;		//教員氏名
+	private String password;	//パスワード 
+	
+	public Teacher(int teacherId, String name, String password) {
+	
+		this.teacherId = teacherId;
+		this.name = name;
+		this.password = password;
+	}
+
+	public int getTeacherId() {
+		return teacherId;
+	}
+	
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void  setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void  setPassword(String password) {
+		this.password = password;
+	}
 }
