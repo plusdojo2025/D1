41ae7e455b602f67d97e2169ecca04e67231e92e
plusdojo2025-aoyagi-kakeya <aoyagi-kakeya-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 15:18:14 +0900

    Update LoginServlet.java
    loginServlet

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -1,5 +1,65 @@
 package servlet;
 
-public class LoginServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.TeachersDAO;
+import dto.Teacher;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String teacherIdStr = request.getParameter("teacherId");
+		String password = request.getParameter("password");
+		
+		int teacherId = Integer.parseInt(teacherIdStr);
+
+		// ログイン処理を行う
+		TeachersDAO iDao = new TeachersDAO();
+		if (iDao.isLoginOK(new Teacher(teacherId, null, password))) { // ログイン成功
+			// セッションスコープにIDを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("teacherId", teacherId);
+
+			// メニューサーブレットにリダイレクトする
+			response.sendRedirect("/webapp/MenuServlet");
+		} else { // ログイン失敗
+			// リクエストスコープに、エラーメッセージを格納する
+			request.setAttribute("errorMessage", "教員IDまたはパスワードに間違いがあります。");
+
+			//　ログインページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
 }
