778d0f1bb866ff65f8667318b7b8f82502e9eed3
plusdojo2025-Nakano <nakano-ayame-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 09:46:50 +0900

    info_student.jsのコメントを追加

--- source/src/main/webapp/js/info_student.js
+++ source/src/main/webapp/js/info_student.js
@@ -1,14 +1,17 @@
 'use strict'
+// チェックボックスのエレメントを取得
 let attendanceCheck = document.getElementById('attendanceCheck');
 let submissionCheck = document.getElementById('submissionCheck');
 let gradesCheck = document.getElementById('gradesCheck');
 let attitudeCheck = document.getElementById('attitudeCheck');
 let interviewCheck = document.getElementById('interviewCheck');
-let attendance = document.getElementById('attendance');
-let submission = document.getElementById('submission');
-let grades = document.getElementById('grades');
-let attitude = document.getElementById('attitude');
-let interview = document.getElementById('interview');
+
+//　表示・非表示を切り替えたいエレメントを取得
+let attendance = document.getElementById('attendance'); // 出欠
+let submission = document.getElementById('submission'); // 提出物
+let grades = document.getElementById('grades'); // 成績
+let attitude = document.getElementById('attitude'); // 授業態度
+let interview = document.getElementById('interview'); // 面談
 
 // 値の変更を捕捉
 attendanceCheck.addEventListener('change', {check: attendanceCheck, name: attendance, handleEvent: changeDisplay});
@@ -16,11 +19,13 @@ submissionCheck.addEventListener('change', {check: submissionCheck, name: submis
 gradesCheck.addEventListener('change', {check: gradesCheck, name: grades, handleEvent: changeDisplay});
 attitudeCheck.addEventListener('change', {check: attitudeCheck, name: attitude, handleEvent: changeDisplay});
 interviewCheck.addEventListener('change', {check: interviewCheck, name: interview, handleEvent: changeDisplay});
+// addEventListenerで変更を検知。変更したタイミングでchangeDisplayを呼出し（handleEvent: の後ろに関数名）
+// checkでチェックボックスのエレメントを、nameで表示切替したい項目の名前を渡すと、関数側で「this.check」「this.name」で取得できる
 
 //　変更時の処理
 function changeDisplay(event) {
 	let element = attendance;
-	// エレメントの変更
+	// 名前に応じてエレメントを設定
 	switch (this.name) {
 		case attendance:
 		element = attendance;
