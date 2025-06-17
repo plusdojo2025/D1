659771297271c81632f42a46444d2107ca1e3d82
plusdojo2025-Nakano <nakano-ayame-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 13:29:40 +0900

    DAOの修正、個人閲覧画面サーブレット

--- source/src/main/java/servlet/InfoStudentServlet.java
+++ source/src/main/java/servlet/InfoStudentServlet.java
@@ -11,9 +11,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.el.parser.ParseException;
+
 import dao.AttendanceRecordsDAO;
+import dao.ClassRoomDAO;
+import dao.InterviewDAO;
 import dao.StudentsDAO;
 import dto.AttendanceRecords;
+import dto.ClassRoom;
+import dto.Interview;
 import dto.Students;
 
 /**
@@ -28,67 +34,69 @@ public class InfoStudentServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
+
+		int subjectId = 1, year = 1, month = 1, studentId = 1;
 		// プルダウンの情報
 		if (request.getParameter("subjectId") != null) {
-			String subject = request.getParameter("subjectId");
-			request.setAttribute("subjectId", subject);
-		} else {
-			request.setAttribute("subjectId", 1);
+			subjectId = Integer.parseInt(request.getParameter("subjectId"));
 		}
-		
+
 		if (request.getParameter("yearId") != null) {
-			String year = request.getParameter("yearId");
-			request.setAttribute("yearId", year);
-		} else {
-			request.setAttribute("yearId", 1);
+			year = Integer.parseInt(request.getParameter("yearId"));
 		}
-		
+
 		if (request.getParameter("monthId") != null) {
-			String month = request.getParameter("monthId");
-			request.setAttribute("monthId", month);
-		} else {
-			request.setAttribute("monthId", 1);
+			month = Integer.parseInt(request.getParameter("monthId"));
 		}
-		
+
+		request.setAttribute("subjectId", subjectId);
+		request.setAttribute("yearId", year);
+		request.setAttribute("monthId", month);
+
 		StudentsDAO stuDAO = new StudentsDAO();
-		
-		String studentId = request.getParameter("studentId");
-		//List<Students> studentsList = stuDAO.select(null);
-		Students st = new Students(1,1,1,1,"山田太郎","やまだたろう","","","");
-		List<Students> studentsList = new ArrayList<Students>();
-		studentsList.add(st);
-		
+
+		if (request.getParameter("studentId") != null) {
+			studentId = Integer.parseInt(request.getParameter("studentId"));
+		}
+		List<Students> studentsList = stuDAO.select(new Students(studentId, -1, -1, -1, "", "", "", "", ""));
+		Students student = new Students();
+		if (studentsList.size() > 0) student = studentsList.get(0);
+
 		AttendanceRecordsDAO arDAO = new AttendanceRecordsDAO();
-		List<AttendanceRecords> arList = arDAO.select(1);
-		
-		request.setAttribute("student", studentsList.get(0));
-		request.setAttribute("className", null); // クラス名
-		
+		List<AttendanceRecords> arList = arDAO.select_Fiscal(-10, student.getClassId(), month, subjectId);
+		request.setAttribute("student", student);
+
+		ClassRoomDAO crDAO = new ClassRoomDAO();
+		List<ClassRoom> classList = crDAO.select(new ClassRoom(student.getClassId(),student.getGrade(),""));
+		request.setAttribute("className", classList.get(0).getClassName());
+
 		// 全体の出席
-		request.setAttribute("attendedRate", null);
-		request.setAttribute("attendedNum", null);
-		request.setAttribute("shouldAttendNum", null);
+		request.setAttribute("attendedRate", arDAO.GetAttendedRate(studentId));
+		request.setAttribute("attendedNum", arDAO.GetAttendedNum(studentId));
+		request.setAttribute("shouldAttendNum", arDAO.GetShouldAttendedNum(studentId));
 
 		// 全体の提出物
 		request.setAttribute("submittedRate", null);
 		request.setAttribute("submittedNum", null);
 		request.setAttribute("shouldSubmitNum", null);
-		
+
 		// 指定した教科の出席・提出物
-		request.setAttribute("subjectAttendedRate", null);
-		request.setAttribute("subjectSubmittedRate", null);
+		request.setAttribute("subjectAttendedRate", arDAO.GetAttendedRate(studentId, subjectId));
 		request.setAttribute("attendanceRecords", arList);
+		request.setAttribute("subjectSubmittedRate", null);
 		request.setAttribute("assignmentsList", null);
-		
+
 		// 成績
 		request.setAttribute("gradesList", null);
 		request.setAttribute("average", null);
-		
+
 		// 面談
-		request.setAttribute("interviewList", null);
-		request.setAttribute("lastInterviewList", null);
-		
+		InterviewDAO itvDAO = new InterviewDAO();
+		List<Interview> itvList = itvDAO.select_thisYear(studentId);
+		List<Interview> lastItvList = itvDAO.select_lastYear(studentId);
+		request.setAttribute("interviewList", itvList);
+		request.setAttribute("lastInterviewList", lastItvList);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/info_student.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -97,45 +105,73 @@ public class InfoStudentServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-StudentsDAO stuDAO = new StudentsDAO();
-		
 		request.setCharacterEncoding("UTF-8");
-		String studentId = request.getParameter("studentId");
-		//List<Students> studentsList = stuDAO.select(null);
-		Students st = new Students(1,1,1,1,"山田太郎","やまだたろう","","","");
-		List<Students> studentsList = new ArrayList<Students>();
-		studentsList.add(st);
-		
+
+		int subjectId = -1, year = -1, month = -1;
+		// プルダウンの情報
+		if (request.getParameter("subjectId") != null) {
+			subjectId = Integer.parseInt(request.getParameter("subjectId"));
+			request.setAttribute("subjectId", subjectId);
+		} else {
+			request.setAttribute("subjectId", 1);
+		}
+
+		if (request.getParameter("yearId") != null) {
+			year = Integer.parseInt(request.getParameter("yearId"));
+			request.setAttribute("yearId", year);
+		} else {
+			request.setAttribute("yearId", 1);
+		}
+
+		if (request.getParameter("monthId") != null) {
+			month = Integer.parseInt(request.getParameter("monthId"));
+			request.setAttribute("monthId", month);
+		} else {
+			request.setAttribute("monthId", 1);
+		}
+
+		StudentsDAO stuDAO = new StudentsDAO();
+
+		int studentId = Integer.parseInt(request.getParameter("studentId"));
+		List<Students> studentsList = stuDAO.select(new Students(studentId, -1, -1, -1, "", "", "", "", ""));
+		Students student = studentsList.get(0);
+
 		AttendanceRecordsDAO arDAO = new AttendanceRecordsDAO();
-		List<AttendanceRecords> arList = arDAO.select(1);
-		
+		List<AttendanceRecords> arList = arDAO.select_Fiscal(-10, student.getClassId(), month, subjectId);
+
 		request.setAttribute("student", studentsList.get(0));
-		request.setAttribute("className", null); // クラス名
-		
+
+		ClassRoomDAO crDAO = new ClassRoomDAO();
+		List<ClassRoom> classList = crDAO.select(new ClassRoom(1,1,""));
+		request.setAttribute("className", classList.get(0).getClassName());
+
 		// 全体の出席
-		request.setAttribute("attendedRate", null);
-		request.setAttribute("attendedNum", null);
-		request.setAttribute("shouldAttendNum", null);
+		request.setAttribute("attendedRate", arDAO.GetAttendedRate(studentId));
+		request.setAttribute("attendedNum", arDAO.GetAttendedNum(studentId));
+		request.setAttribute("shouldAttendNum", arDAO.GetShouldAttendedNum(studentId));
 
 		// 全体の提出物
 		request.setAttribute("submittedRate", null);
 		request.setAttribute("submittedNum", null);
 		request.setAttribute("shouldSubmitNum", null);
-		
+
 		// 指定した教科の出席・提出物
-		request.setAttribute("subjectAttendedRate", null);
-		request.setAttribute("subjectSubmittedRate", null);
+		request.setAttribute("subjectAttendedRate", arDAO.GetAttendedRate(studentId, subjectId));
 		request.setAttribute("attendanceRecords", arList);
+		request.setAttribute("subjectSubmittedRate", null);
 		request.setAttribute("assignmentsList", null);
-		
+
 		// 成績
 		request.setAttribute("gradesList", null);
 		request.setAttribute("average", null);
-		
+
 		// 面談
-		request.setAttribute("interviewList", null);
-		request.setAttribute("lastInterviewList", null);
-		
+		InterviewDAO itvDAO = new InterviewDAO();
+		List<Interview> itvList = itvDAO.select_thisYear(studentId);
+		List<Interview> lastItvList = itvDAO.select_lastYear(studentId);
+		request.setAttribute("interviewList", itvList);
+		request.setAttribute("lastInterviewList", lastItvList);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/info_student.jsp");
 		dispatcher.forward(request, response);
 	}
